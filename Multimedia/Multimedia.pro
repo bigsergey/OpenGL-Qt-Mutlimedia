ce722fdd8d7cdef8154aec6a6119484790bdbd2b
Dawid <dawidss7@gmail.com>
2014-06-07 11:31:36 +0200

    Main widget

--- Multimedia/Multimedia.pro
+++ Multimedia/Multimedia.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        window.cpp
+        window.cpp \
+    mainwidget.cpp
 
-HEADERS  += window.h
+HEADERS  += window.h \
+    mainwidget.h
 
 FORMS    += window.ui
---
+++ Multimedia/mainwidget.h
@@ -0,0 +1,18 @@
+#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
+
+#include <QGLWidget>
+
+class MainWidget : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit MainWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // MAINWIDGET_H
